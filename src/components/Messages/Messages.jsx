1978a4073698b970f61370838084dd0527ef318c
Your Name <goncharovigor12@gmail.com>
2023-01-14 21:58:41 +0300

    messages

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import classes from "./Messages.module.scss";
 import DialogItem from "./DialogItem";
 import MessageItem from "./MessageItem";
---
+++ src/state/state.js
@@ -0,0 +1,25 @@
+const state = {
+  dialogsData: [
+    { id: 1, name: "Igor" },
+    { id: 2, name: "Vadim" },
+    { id: 3, name: "Dima" },
+    { id: 4, name: "Ilya" },
+    { id: 5, name: "Vasya" },
+  ],
+  messagesData: [
+    { id: 1, message: "Hi" },
+    { id: 2, message: "lol" },
+    { id: 3, message: "Hello world" },
+    { id: 4, message: "Im here" },
+    { id: 5, message: "i Vasya shiiit" },
+  ],
+  postsData: [
+    { id: 1, user: "Igor", text: "post1", likesCount: 2 },
+    { id: 2, user: "Igor", text: "post2", likesCount: 6 },
+    { id: 3, user: "Igor", text: "post3", likesCount: 42 },
+    { id: 4, user: "Igor", text: "post4", likesCount: 77 },
+    { id: 5, user: "Igor", text: "post5", likesCount: 112 },
+  ],
+};
+
+export default state;
